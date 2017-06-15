67cf4ff474e3ae41ad49139919e7935110c6789f
Bryam Rodriguez <brodriguezayabr@gmail.com>
2017-06-15 15:04:58 -0500

    Initial setup

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  createStore,
+  applyMiddleware,
+} from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const store = createStore(
+  reducers,
+  applyMiddleware(),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
